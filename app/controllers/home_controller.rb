e6a5a77f94a9402408c087897c3697001e065ce2
Santo DeRose <derosesanto95@gmail.com>
2021-04-14 15:10:25 -0500

    added background

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,38 @@
+class HomeController < ApplicationController
+  def index
+require 'net/http'
+require 'json'
+
+
+
+# News Data
+@url = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN'
+@uri = URI(@url)
+@response = Net::HTTP.get(@uri)
+@news = JSON.parse(@response)
+
+
+# Price Data
+@prices_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,BCH,EOS,LTC,ADA,XML,MIOTA,USDT,TRX&tsyms=USD'
+@prices_uri = URI(@prices_url)
+@prices_response = Net::HTTP.get(@prices_uri)
+@prices = JSON.parse(@prices_response)
+end
+
+def prices
+	require 'net/http'
+	require 'json'
+
+
+	@symbol = params[:sym]
+if @symbol
+	@symbol = @symbol.upcase
+
+	# Price Data
+@quote_url = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=' + @symbol + '&tsyms=USD'
+@quote_uri = URI(@quote_url)
+@quote_response = Net::HTTP.get(@quote_uri)
+@quote = JSON.parse(@quote_response)
+end
+end
+end
